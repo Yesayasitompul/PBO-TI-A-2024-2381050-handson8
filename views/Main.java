47685a674f5d20d279d6735b9ba4030eec943caa
Yesaya Sitompul <2381050@unai.edu>
2024-11-08 08:02:30 +0700

    Handson8

--- views/Main.java
+++ views/Main.java
@@ -1,5 +1,16 @@
 package views;
 
+import repositories.TodoListRepository;
+import repositories.TodoListRepositoryImpl;
+import services.TodoListService;
+import services.TodoListServiceImpl;
+
 public class Main {
+    public static void main(String[] args) {
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListTerminalView(todoListService);
+        todoListView.run();
+    }
 
 }
